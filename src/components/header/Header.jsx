470bbd2cd0c267bd1e4bb2ea85206a9ef44c2921
Adam Perez <amperez0208@gmail.com>
2022-06-02 13:27:27 -0400

    added header socials cmpnt and react icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,7 +11,13 @@ const Header = () => {
     <h1>Adam</h1>
     <h5 className='text-light'>Fullstack Developer</h5>
     <CTA/>
+    <HeaderSocials/>
 
+
+    <div className='me'>
+      <img src='' alt=''/>
+    </div>
+    <a href='#contact' className='scroll__down'>Scroll Down</a>
 
       </div>
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsYoutube} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+<a href='https://www.linkedin.com/in/adamperez86/' target='-blank'><BsLinkedin/></a>
+<a href='https://github.com/APxISO' target='-blank'><BsGithub/></a>
+<a href='https://www.Dribble.com/' target='-blank'><BsYoutube/></a>
+
+
+    </div>
+  )
+}
+
+export default HeaderSocials
