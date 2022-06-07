721fe91503fed722522458ef618ccb41d8dc8969
Adam Perez <amperez0208@gmail.com>
2022-06-07 08:21:45 -0400

    contact css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,21 +12,21 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <MdOutlineMail/>
+            <MdOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>AMPerez0208@gmail.com</h5>
             <a href='mailto:AMPerez0208@gmail.com' target='_blank'>Send a Message</a>
           </article>
 
           <article className='contact__option'>
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Adam Perez</h5>
             <a href='https://m.me/IAmAdamPerez' target='_blank'>Send a Message</a>
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
             <a href='https://api.whatsapp.com/send?phone+19043334729' target='_blank'>Send a Message</a>
