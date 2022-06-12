5193f0c9e9e66162685ebea985c0fab24e33d86a
Adam Perez <amperez0208@gmail.com>
2022-06-12 18:39:24 -0400

    added my pics lol

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
-import ME from '../../assets/avatarM2.png'
+import ME from '../../assets/APSelfie3-removebg-preview.png'
 
 const Header = () => {
   return (
